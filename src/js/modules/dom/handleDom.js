6a4ac08a42b44ae374bda422544797c7df835ff2
Nicolas <devnicolas.coroa@gmail.com>
2026-03-31 22:19:11 -0300

    :sparkles: feat: integrate form validation and submission handling

--- src/js/modules/dom/handleDom.js
+++ src/js/modules/dom/handleDom.js
@@ -1,4 +1,6 @@
 import { initInputDate, initInputTime } from "./initUi"
+import { inputsValidation } from "../form/validation"
+import { clearScheduleForm } from "../dom/renders"
 
 const wrappersDate = document.querySelectorAll(".date-picker")
 const wrapperTime = document.querySelector(".datetime-picker")
@@ -6,6 +8,7 @@ const wrapperTime = document.querySelector(".datetime-picker")
 const newScheduleButton = document.querySelector(".footer__schedule")
 
 const modalCloseButton = document.querySelector(".modal__button-close")
+const form = document.querySelector(".modal__form")
 
 newScheduleButton.addEventListener("click", () => {
   let modal = document.querySelector(".modal")
@@ -15,10 +18,21 @@ newScheduleButton.addEventListener("click", () => {
 })
 
 modalCloseButton.addEventListener("click", () => {
-  let modal = document.querySelector(".modal")
-  let overlay = document.querySelector(".app-overlay")
-  modal.classList.toggle("visible")
-  overlay.classList.toggle("visible")
+  const modal = document.querySelector(".modal")
+  const overlay = document.querySelector(".app-overlay")
+  const formInputs = document.querySelectorAll(".modal__form input")
+  const formTextarea = document.querySelector(".modal__form textarea")
+  const fields = [...formInputs, formTextarea]
+
+  modal.classList.remove("visible")
+  overlay.classList.remove("visible")
+
+  clearScheduleForm(fields)
+})
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
+  inputsValidation()
 })
 
 document.addEventListener("DOMContentLoaded", () => {
