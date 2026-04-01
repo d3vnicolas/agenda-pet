8f197a678a857cf878d669e56d907a7f4061215c
Nicolas <devnicolas.coroa@gmail.com>
2026-03-31 22:17:23 -0300

    :sparkles: feat: implement form input validation logic

--- src/js/modules/form/validation.js
+++ src/js/modules/form/validation.js
@@ -1,8 +1,66 @@
-function inputsValidation() {
-  const inputs = document.querySelectorAll(".input")
+import { removeFormInputError, renderFormInputError } from "../dom/renders"
 
-  inputs.forEach((input) => {
-    if (input.name === "name") {
-    }
+export function inputsValidation() {
+  const inputs = document.querySelectorAll(".modal__form input")
+  const textArea = document.querySelector(".modal__form textarea")
+  const fields = [...inputs, textArea]
+
+  fields.forEach((field) => {
+    whichInput(field.getAttribute("name"), field)
   })
+}
+
+function whichInput(attr, field) {
+  const result = {
+    tutor: nameValidation,
+    petName: petNameValidation,
+    phone: phoneValidation,
+    service: descriptionValidation,
+  }
+  const fn = result[attr]
+  return fn ? fn(field) : null
+}
+
+function nameValidation(field) {
+  const regex = /^[A-Za-zÀ-ÿ]+(?:\s+[A-Za-zÀ-ÿ]+)+$/
+  let result = true
+
+  result = regex.test(field.value)
+  !result
+    ? renderFormInputError(field.parentNode, "Nome e/ou sobrenome inválido")
+    : removeFormInputError(field.parentNode)
+}
+
+function petNameValidation(field) {
+  const regex = /^(?=(?:.*[A-Za-zÀ-ÿ]){3,})[A-Za-zÀ-ÿ]+(?:\s+[A-Za-zÀ-ÿ]+)*$/
+  let result = true
+
+  result = regex.test(field.value)
+  !result
+    ? renderFormInputError(field.parentNode, "Nome do PET inválido")
+    : removeFormInputError(field.parentNode)
+}
+
+function phoneValidation(field) {
+  const regex = /^\(?\d{2}\)?\s?9\d{4}-?\d{4}$/
+  let result = true
+
+  result = regex.test(field.value)
+  !result
+    ? renderFormInputError(field.parentNode, "Telefone inválido")
+    : removeFormInputError(field.parentNode)
+
+  return result
+}
+
+function descriptionValidation(field) {
+  const regex = /^\S+\s+\S+.*$/
+  let result = true
+
+  result = regex.test(field.value)
+  !result
+    ? renderFormInputError(field.parentNode, "Descrição inválida")
+    : removeFormInputError(field.parentNode)
+
+  return result
 }
